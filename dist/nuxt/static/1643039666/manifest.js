54e0b20f9d278caaccfa26e6a98210b9ec596672
Robin BERGERE <robin.bergere@gmail.com>
2022-01-25 19:24:35 +0100

    build

--- dist/nuxt/static/1643039666/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fhelp","\u002F","\u002Fplay","\u002Ftuner","\u002Foptions","\u002Fabout","\u002Fseq"]})
---
+++ dist/nuxt/static/1643135059/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Foptions","\u002Fhelp","\u002Fseq","\u002Ftuner","\u002Fabout","\u002Fplay"]})
